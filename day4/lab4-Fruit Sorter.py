a27b244bba0a8bf79dd6d7dc6458697dfd64b7d3
simon21-meet <simon21@meet.mit.edu>
2019-07-24 09:52:44 -0400

    Stopping here

---
+++ day4/lab4-Fruit Sorter.py
@@ -0,0 +1,11 @@
+fruit=input("What fruit do you want to sort?")
+if fruit.lower()=="apples":
+    print("Apples go in bin num. 1")
+elif fruit.lower()=='oranges':
+    print("Oranges go in bin 2")
+elif fruit.lower()=="bananas":
+    print("Bananas go in bin 3")
+else:
+    print('Error! I do not recognize this fruit!')
+
+    
