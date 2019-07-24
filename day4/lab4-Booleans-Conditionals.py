446e1ea3f3f2f50deecf644eb970ca81866e96c8
simon21-meet <simon21@meet.mit.edu>
2019-07-24 09:32:28 -0400

    Stopping here

---
+++ day4/lab4-Booleans-Conditionals.py
@@ -0,0 +1,22 @@
+print(10==11)
+print("strings"=='strings')
+print('strings'=="strings")
+
+#Part 2: Boolean Operators:
+print(1>-1)
+print('MEET'=='meet')
+print("meet"=='meet')
+a=3
+b=10
+print(a*4>2*b)
+
+#Part 3: Conditional statement:
+tab=True
+if tab:
+    print('chocolate')
+print('Tabs are cool')
+
+tab=False
+if tab:
+    print('chocolate')
+    print('Tabs are cool!')
