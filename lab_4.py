43b888e4dbc483cdcf3cca4c06bd5553153b153b
simon21-meet <simon21@meet.mit.edu>
2019-07-24 10:02:55 -0400

    Stopping here

---
+++ lab_4.py
@@ -0,0 +1,23 @@
+#script_a.py:
+num1 = 2
+num2 = 3
+num3 = 1
+
+if num1 > num2:
+    print('chocolate')
+elif num1 > num3:
+    print('vanilla')
+else:
+    print('strawberry')
+
+#script_b.py
+num1 = 2
+num2 = 3
+num3 = 1
+
+if num1 > num3:
+    print('chocolate')
+elif num1 > num2:
+    print('vanilla')
+else:
+    print('strawberry')
