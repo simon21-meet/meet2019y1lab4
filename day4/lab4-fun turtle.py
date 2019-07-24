0ca2902e16859c12064d249fe25e32a2ce32abbc
simon21-meet <simon21@meet.mit.edu>
2019-07-24 03:06:48 -0400

    Stopping here

---
+++ day4/lab4-fun turtle.py
@@ -0,0 +1,23 @@
+import turtle
+turtle.shape("square")
+turtle.shape("turtle")
+finn=turtle.clone()
+finn.shape("square")
+finn.goto(100,100)
+finn.goto(100,0)
+finn.goto(0,0)
+finn.goto(0,100)
+finn.goto(100,100)
+charlie=turtle.clone()
+charlie.shape("triangle")
+charlie.goto(50,50)
+charlie.goto(100,0)
+finn.penup()
+finn.goto(200,200)
+finn.pendown()
+finn.stamp()
+finn.goto(300,300)
+finn.clearstamps()
+turtle.mainloop()
+
+
